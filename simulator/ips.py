5ff970ff11834434cb698209157d66a03724d0d3
priamai <paolo@priam.ai>
2022-09-01 22:55:06 +0100

    More refactoring.

---
+++ simulator/ips.py
@@ -0,0 +1,88 @@
+import random
+import urllib.request, json
+from datetime import datetime
+import time
+from faker import Faker
+import yaml
+import sys
+import pprint
+from loader import MessageGenerator
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+cisa_url = 'https://www.cisa.gov/sites/default/files/feeds/known_exploited_vulnerabilities.json'
+
+class ProtoBuf:
+    '''
+    This models a proto buf simple payload
+    '''
+    def __init__(self,**kwargs):
+        self._attributes = kwargs
+
+    def get_field(self,name):
+        if name in self._attributes:
+            return self._attributes[name]
+        else:
+            raise Exception('Field is missing')
+    def get_fields(self):
+        return self._attributes.keys()
+
+    def __str__(self):
+        return str(self._attributes)
+
+DB_URL = "typedb.westeurope.cloudapp.azure.com:1729"
+DB_NAME = "omnibuscyber"
+import argparse
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--reset", help="reset the database",action="store_true")
+    parser.add_argument("--eps", help="events per second", default=1,type=int)
+    parser.add_argument("--total", help="total events", default=1, type=int)
+
+    args = parser.parse_args()
+
+    # Read YAML file
+    with open("config.yaml", 'r') as stream:
+        config_data = yaml.safe_load(stream)
+        folder = Path(__file__).parent.parent.joinpath('schema')
+        mg = MessageGenerator(uri=DB_URL,database=DB_NAME,schema_folder=folder,reset=args.reset)
+        mg.set_mapping(config_data)
+
+    if args.eps > 0:
+        with urllib.request.urlopen(cisa_url) as url:
+            data = json.loads(url.read().decode())
+            logger.info(f"Total vulnerabilities {data['count']}")
+
+            # pick a vulnerability from 2022
+            vulns = data['vulnerabilities']
+
+            cves = [v['cveID'] for v in vulns if v['cveID'].startswith('CVE-2022')]
+
+            rand_cves = random.choices(cves, k=10)
+            running = True
+            past = datetime.now()
+
+            fake = Faker()
+            Faker.seed(0)
+
+            totals = 0
+            logger.info(f'Begin simulation loop with eps = {args.eps}')
+            while running:
+                now = datetime.now()
+                delta_sec = (now - past).total_seconds()
+                if delta_sec > 1:
+                    logger.info(f'Step {now}')
+                    events = []
+                    for x in range(1,args.eps+1):
+                        ips_event = ProtoBuf(hostname=fake.hostname(0),cve=random.choices(cves, k=1)[0],count=random.randint(1,100),timestamp=now)
+                        events.append(ips_event)
+                    mg.generate_queries(events)
+                    totals += len(events)
+                if totals>=args.total: running = False
+                past = now
+                time.sleep(1)
+
---
+++ simulator/loader.py
@@ -0,0 +1,148 @@
+import logging
+from typedb.client import *
+from typedb_query_builder.typedb_query_builder import TypeDBQueryBuilder
+import random
+import string
+import re
+from pathlib import Path
+import os
+import yaml
+
+# create and configure main logger
+logger = logging.getLogger(__name__)
+
+class MessageGenerator:
+
+    def __init__(self, uri, database='omnitest',schema_folder=Path(__file__).parent,reset=False):
+        self._client = TypeDB.core_client(uri)
+        self._database = database
+
+        if reset: self._client.databases().get(database).delete()
+
+        if self._client.databases().contains(database):
+            self.create_schema(schema_folder)
+
+        else:
+            self._client.databases().create(database)
+            self.create_schema(schema_folder)
+
+    def create_schema(self,schema_folder:Path):
+
+        # Read YAML file
+        with open(schema_folder.joinpath('order.yaml'), 'r') as stream:
+            list_tql = yaml.safe_load(stream)
+
+            with self._client.session(self._database, SessionType.SCHEMA) as session:
+                for file_name in list_tql['base']:
+                    with open(os.path.join(schema_folder,file_name), 'r') as schema:
+                        define_query = schema.read()
+                        with session.transaction(TransactionType.WRITE) as transaction:
+                            transaction.query().define(define_query)
+                            transaction.commit()
+                            logger.info(f"Loaded {file_name} into {self._database}")
+
+                for file_name in list_tql['extensions']:
+                    with open(os.path.join(schema_folder,file_name), 'r') as schema:
+                        define_query = schema.read()
+                        with session.transaction(TransactionType.WRITE) as transaction:
+                            transaction.query().define(define_query)
+                            transaction.commit()
+                            logger.info(f"Loaded {file_name} into {self._database}")
+
+    def set_mapping(self,mapping):
+        self._mapping = mapping
+        self.load_schemas()
+
+    def escape_string(self,string_text):
+        return re.sub('\W+', ' ', string_text)
+        #return re.sub('[A-Za-z0-9\s]+','', string_text)
+
+    def load_schemas(self):
+        with self._client.session(self._database, SessionType.SCHEMA) as session:
+            with session.transaction(TransactionType.READ) as tx:
+                entities = set()
+                for field in self._mapping['entities']:
+                    atom = self._mapping['entities'][field]
+                    entities.add(atom['entity'])
+
+                self._schema = {}
+                for entity_type in entities:
+                    entity_prop  = {}
+                    for value_type in ['string','long','boolean','datetime']:
+                        q_att = f'match $s type {entity_type}, owns $a;\
+                        $a sub attribute, value {value_type};\
+                        get $s,$a;'
+
+                        for a in tx.query().match(q_att):
+                            attr= a.map().get("a").get_label()
+                            entity_prop[str(attr)]=value_type
+
+                    self._schema[entity_type] = entity_prop
+                logger.info(f"Loaded {len(self._schema)} entities from config mappings")
+
+    def check_var(self,name:str,var_list:list):
+        if name not in var_list:
+            return name
+        else:
+            new_var = name + str(random.randint(0, 9))
+            return self.check_var(new_var,var_list)
+
+    def generate_queries(self,messages=[]):
+        total_inserts = 0
+
+        for message in messages:
+            tqb = TypeDBQueryBuilder()
+            queries = {}
+            vars = []
+            for field in message.get_fields():
+                if field in self._mapping['entities']:
+                    atom = self._mapping['entities'][field]
+                    # load schema properties
+                    property_types = self._schema[atom['entity']]
+                    if atom['entity'] in queries:
+                        old_ql = queries[atom['entity']]
+                        old_ql.has(atom[field], message.get_field(field),property_types[atom['attribute']])
+                        queries[atom['entity']] = old_ql
+                    else:
+                        var_name = self.check_var(atom['entity'][0],vars)
+
+                        insert_ql = tqb.insert_entity(atom['entity'], var_name)
+
+                        insert_ql.has(atom['attribute'], message.get_field(field),property_types[atom['attribute']])
+                        queries[atom['entity']] = insert_ql
+
+                        vars.append(var_name)
+
+                if field in self._mapping['relations']:
+                    atom = self._mapping['relations'][field]
+
+                    var_name = self.check_var(atom['relation'][0], vars)
+
+                    insert_rel = tqb.insert_relationship(atom['relation'], var_name)
+                    # recover the variables now
+                    for role in atom['roles']:
+                        # need to find the original variable ...
+                        insert_rel.relates(atom['roles'][role], queries[role])
+
+                    #TODO: needs a fix in the building library sadly .... for now workaround
+                    tstamp = message.get_field(field).replace(microsecond=0).isoformat()
+                    insert_rel.has(atom['attribute'], tstamp,"long")
+                    vars.append(var_name)
+                    queries[atom['relation']] = insert_rel
+
+            with self._client.session(self._database, SessionType.DATA) as session:
+                with session.transaction(TransactionType.WRITE) as tx:
+                    tqb.compile_query()
+                    final_query = tqb.get_query()  # Get query
+                    logger.debug(final_query)
+                    insert_iterator = tx.query().insert(final_query)
+
+                    for v in vars:
+                        concepts = [ans.get(v) for ans in insert_iterator]
+                        total_inserts += len(concepts)
+
+                    tx.commit()
+
+        logger.debug(f'Total inserted objects {total_inserts}')
+
+        return total_inserts
